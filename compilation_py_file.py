8cee81322d0aaf5cfb28784e1bc95c6e1cfd3301
Dima <35930590+Cmertho@users.noreply.github.com>
2018-01-31 19:13:10 +0500

    setup.py
    This script compiles python file to exe format

---
+++ compilation_py_file.py
@@ -0,0 +1,16 @@
+from cx_Freeze import setup, Executable
+import os,sys
+os.environ['TCL_LIBRARY'] = r'C:\Users\username\AppData\Local\Programs\Python\Python36-32\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\username\AppData\Local\Programs\Python\Python36-32\tcl\tk8.6'
+build_exe_options = {"packages": ["os"], "excludes": ["PyQt5"]}
+sys.argv.append("build")
+filename = ""
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+setup(
+    name = "names",
+    version = "0.1",
+    description = "title",
+    executables = [Executable(filename, base=base)])
