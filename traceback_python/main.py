ff9c6f3a586f87da5df37cf4bf34df609d2d1992
Dima <35930590+Cmertho@users.noreply.github.com>
2018-07-26 22:11:48 +0500

    Create main.py

---
+++ traceback_python/main.py
@@ -0,0 +1,10 @@
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    import traceback
+    text += ''.join(traceback.format_tb(tb))
+
+    print(text)
+    sys.exit(1)
+
+import sys
+sys.excepthook = log_uncaught_exceptions
